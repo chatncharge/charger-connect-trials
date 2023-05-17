9ac9d8912c585a9ad2e3259acc10bc54cb625101
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-05-17 09:59:52 +0530

    Update Scan to take IP parameter.

--- Scan.java
+++ Scan.java
@@ -3,7 +3,11 @@ import java.net.*;
 
 public class Scan {
     public static void main(final String... args) {
-        String ip = "192.168.8.9";
+        String ip;
+        if (args.length > 0)
+            ip = args[0];
+        else
+            ip = "192.168.8.9";
         for (int i = 0; i < 65535; i++) {
             int j = i;
             new Thread(() -> {
