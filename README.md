93510fcbec78084fb0179241dcdd655a3239c92d
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-05-17 09:50:39 +0530

    Update port range.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Process
 - Dev pushes test code **AND BINARY**
 - Ops pulls repo and runs
+
+LAPTOP-P2K8R4D4
+TIZEN
+6E:94:8F:CB:42:E2
+
+ESP_A1E2E5
+MAC: 30:83:98:A1:E2:E5
+192.168.8.9
--- Scan.java
+++ Scan.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class Scan {
     public static void main(final String... args) {
         String ip = "192.168.8.9";
-        for (int i = 1; i < 16384; i++) {
+        for (int i = 0; i < 65535; i++) {
             int j = i;
             new Thread(() -> {
                 try {
